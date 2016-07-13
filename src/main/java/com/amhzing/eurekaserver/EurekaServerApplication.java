b434b8b90d29d96463136f6371e448786a23f1ec
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-07-13 20:37:30 +0200

    Slight updates

--- src/main/java/com/amhzing/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/amhzing/eurekaserver/EurekaServerApplication.java
@@ -1,7 +1,7 @@
 package com.amhzing.eurekaserver;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
@@ -9,6 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+        new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
 	}
 }
